598d68e5d9f7f6fa68e1a95d53c192ba24345bd1
Ravan Scafi <ravan.scafi@hellofresh.com>
2022-12-06 12:13:54 +0100

    Solve Day 06

---
+++ day06/part2/main.go
@@ -0,0 +1,50 @@
+// https://adventofcode.com/2022/day/6
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader, err := os.Open("day06/input-challenge.txt")
+	defer reader.Close()
+	if err != nil {
+		fmt.Printf("failed to open input file: %s\n", err)
+	}
+
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanRunes)
+
+	buffer := ""
+	markerSize := 14
+	marker := -1
+
+	for scanner.Scan() {
+		marker++
+		if len(buffer) < markerSize {
+			buffer += scanner.Text()
+			continue
+		}
+
+		found := true
+		for i, r := range buffer {
+			if index := strings.Index(buffer[i+1:], string(r)); index >= 0 {
+				buffer = buffer[i:] // repeated char will be cut below
+				found = false
+				break
+			}
+		}
+
+		if found {
+			break
+		}
+
+		buffer = buffer[1:] + scanner.Text()
+	}
+
+	fmt.Println(buffer)
+	fmt.Println(marker)
+}
